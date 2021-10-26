0d8b068948f465fc60faf4b41033b3d8de0bdc5a
Abderrahmane Fadili <abdouelrfadili@gmail.com>
2021-10-26 10:57:55 +0100

    create models/post.php

---
+++ models/Post.php
@@ -0,0 +1,40 @@
+<?php
+class Post
+{
+    //DB stuff
+    private $conn;
+    private $table = 'posts';
+
+    //Post properties
+    public  $id;
+    public $category_id;
+    public $category_title;
+    public $title;
+    public $body;
+    public $author;
+    public $createdAt;
+
+    //Constructor with DB
+    public function __construct($db)
+    {
+        $this->conn = $db;
+    }
+
+    //get posts
+    public function read()
+    {
+        //Create Query
+        $query = 'SELECT p.id,c.title 
+                  AS category_title,p.title,p.body,p.author,p.createdAt
+                  FROM ' . $this->table . ' p INNER JOIN categories c 
+                  ON p.category_Id = c.id ORDER BY p.id DESC';
+
+        //Prepare Statement
+        $stmt = $this->conn->prepare($query);
+
+        // Execut Query
+        $stmt->execute();
+
+        return $stmt;
+    }
+}
